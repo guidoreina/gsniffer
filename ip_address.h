e9e58f49d9d8a75967d8d4af71bb4744cf4b34bd
Guido Reina <guido.reina@yahoo.com>
2012-08-02 19:43:15 +0200

    New: now it is possible to dump the connections to a file upon reception of SIGUSR1.

--- ip_address.h
+++ ip_address.h
@@ -9,8 +9,11 @@ struct ip_address {
 	bool operator<(const ip_address& other) const;
 	bool operator<=(const ip_address& other) const;
 	bool operator==(const ip_address& other) const;
+	bool operator!=(const ip_address& other) const;
 	bool operator>(const ip_address& other) const;
 	bool operator>=(const ip_address& other) const;
+
+	static int compare(const ip_address& ip1, const ip_address& ip2);
 };
 
 inline bool ip_address::operator<(const ip_address& other) const
@@ -28,6 +31,11 @@ inline bool ip_address::operator==(const ip_address& other) const
 	return ipv4 == other.ipv4;
 }
 
+inline bool ip_address::operator!=(const ip_address& other) const
+{
+	return ipv4 != other.ipv4;
+}
+
 inline bool ip_address::operator>(const ip_address& other) const
 {
 	return ipv4 > other.ipv4;
@@ -36,6 +44,22 @@ inline bool ip_address::operator>(const ip_address& other) const
 inline bool ip_address::operator>=(const ip_address& other) const
 {
 	return ipv4 >= other.ipv4;
+}
+
+inline int ip_address::compare(const ip_address& ip1, const ip_address& ip2)
+{
+	const unsigned char* s1 = (const unsigned char*) &ip1.ipv4;
+	const unsigned char* s2 = (const unsigned char*) &ip2.ipv4;
+
+	for (unsigned i = 0; i < sizeof(u_int32_t); i++) {
+		if (s1[i] < s2[i]) {
+			return -1;
+		} else if (s1[i] > s2[i]) {
+			return 1;
+		}
+	}
+
+	return 0;
 }
 
 #endif // IP_ADDRESS_H
